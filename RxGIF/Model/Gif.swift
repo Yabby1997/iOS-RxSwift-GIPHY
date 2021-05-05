3e34159b1c073e07a3520cf665ff5f430b7ee014
yabby <yabby1997@gmail.com>
2021-05-06 00:55:40 +0900

    DetailView 개선

---
+++ RxGIF/Model/Gif.swift
@@ -0,0 +1,17 @@
+//
+//  Gif.swift
+//  RxGIF
+//
+//  Created by Seunghun Yang on 2021/05/06.
+//
+
+import Foundation
+
+struct Gif {
+    let id: String              // id
+    let title: String           // title
+    let source: String          // source_tld
+    let trendingDate: String    // trending_datetime
+    let thumbnailURL: String    // images/downsized_small
+    let originalURL: String     // images/original
+}
